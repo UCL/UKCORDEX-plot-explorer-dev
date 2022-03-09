6ee18cb61bdb5f3af8a1a3d8b764fea86de14838
acholyn <amandaholyn@gmail.com>
2022-03-09 12:10:07 +0000

    amended margins

--- src/components/PlotExplorerContainer.js
+++ src/components/PlotExplorerContainer.js
@@ -80,7 +80,7 @@ function PlotExplorerContainer() {
       <Row>
         {warnings.length >= 1 && <DisplayWarnings warnings={warnings} />}
       </Row>
-      <Row>
+      <Row style={{ marginTop: 10 }}>
         <Col xs={2} className="ml-2 position-sticky">
           <Stack gap={1} style={{ position: "fixed" }}>
             <Container>
