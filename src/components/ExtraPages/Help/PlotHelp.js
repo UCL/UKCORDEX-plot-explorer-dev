5222ac2a265e0d57b33f45065d4bc783e3ded9a4
acholyn <amandaholyn@gmail.com>
2022-03-11 15:41:43 +0000

    populated plot help page

--- src/components/ExtraPages/Help/PlotHelp.js
+++ src/components/ExtraPages/Help/PlotHelp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Image } from "react-bootstrap";
 import { PlotInfoHead, PlotInfoContents } from "./Contents/PlotInfo";
 import "../ExtraPages.css";
 
@@ -9,14 +9,28 @@ export default function PlotHelp() {
       <div>
         {/* intro section */}
         <h1>{PlotInfoHead.title}</h1>
-        <p>{PlotInfoHead.intro}</p>
+        <p>
+          {PlotInfoHead.intro} <a href={PlotInfoHead.links[0]}>this page</a>
+        </p>
+        <p>
+          For guidance on how to use these plots, see{" "}
+          <a href={PlotInfoHead.links[1]}>our help page</a>{" "}
+        </p>
       </div>
       <br></br>
+      <p>{PlotInfoHead.overview} </p>
+      <Image
+        fluid="true"
+        src={`${process.env.PUBLIC_URL}${PlotInfoHead.legend}`}
+        alt=""
+      />
       {/* section about the plots */}
-      {PlotInfoContents.map(({ plot, description, i }) => (
+      {PlotInfoContents.map((plot, i) => (
         <div key={i}>
-          <h5>{plot}</h5>
-          <p>{description}</p>
+          <h5>{plot.plot}</h5>
+          {plot.description.map((desc, i) => (
+            <p key={i}>{desc}</p>
+          ))}
           <br></br>
         </div>
       ))}
