7a16198ac5d7e144f04818eafdd5ae24e37ff9be
acholyn <amandaholyn@gmail.com>
2022-03-11 10:03:10 +0000

    updated about with hardcoded text for hyperlinks

--- src/components/ExtraPages/About/About.js
+++ src/components/ExtraPages/About/About.js
@@ -8,23 +8,27 @@ export default function About() {
     <Container className="about">
       <div>
         <h1>{AboutInfo.title}</h1>
-        {AboutInfo.contents.map((paragraph, i) => (
-          <p key={i}>{paragraph}</p>
-        ))}
-        <p style={{ textAlign: "center" }}>
-          <span
-            style={{
-              paddingRight: 20,
-            }}>
-            <a href={AboutInfo.links[1]}>Met Office Climate Projections </a>
-          </span>
-          <span
-            style={{
-              paddingLeft: 20,
-            }}>
-            <a href={AboutInfo.links[2]}>EuroCORDEX Ensemble</a>
-          </span>
+        <p>
+          <a href={AboutInfo.links[1]}>
+            The Met Office's UK Climate Projections (UKCP18)
+          </a>
+          provide several different products giving information about the future
+          climate of the UK, including projections at global (60km), regional
+          (12km) and local (2.2km) scales. Although the regional projections are
+          proving beneficial in understanding future climate risks, these were
+          derived by perturbing the physical parameters of a single global
+          climate model (GCM) and a single regional climate model (RCM), and so
+          may not sample the full range of possible futures that is consistent
+          with current scientific understanding. A more comprehensive assessment
+          of potential climate futures for the UK can be obtained by
+          supplementing the UKCP18 regional projections with information from{" "}
+          <a href={AboutInfo.links[2]}>the EuroCORDEX ensemble</a> at the same
+          12km resolution. This combined set will allow a better understanding
+          of structural uncertainty in UK climate projections and substantially
+          enhances the information base available to support future climate
+          change risk assessments.
         </p>
+        <p>{AboutInfo.contents[1]}</p>
       </div>
     </Container>
   );
