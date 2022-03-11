715db18c34ab2ab3aade52eec419b4ec7759e32c
acholyn <amandaholyn@gmail.com>
2022-03-11 13:48:10 +0000

    populated indices help from overleaf
    added react-latex to help with formatting
    added some images for more complex demonstrations
    added some styling

--- src/components/ExtraPages/Help/IndicesHelp.js
+++ src/components/ExtraPages/Help/IndicesHelp.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import { IndicesInfoContents, IndicesInfoHead } from "./Contents/IndicesInfo";
+import { Container, Table, Row, Col, Image } from "react-bootstrap";
+import {
+  CoreVarsContents,
+  IndicesInfoContents,
+  IndicesInfoHead,
+} from "./Contents/IndicesInfo";
 import "../ExtraPages.css";
+import Latex from "react-latex";
 
 function IndicesHelp() {
+  // var Latex = require("react-latex");
   return (
     <Container className="help">
       <div>
@@ -12,12 +18,76 @@ function IndicesHelp() {
         <p>{IndicesInfoHead.intro}</p>
       </div>
       <br></br>
-      {/* section about the indices */}
-      {IndicesInfoContents.map(({ index, description, i }) => (
-        <div key={i}>
-          <h5>{index}</h5>
-          <p>{description}</p>
-        </div>
+      <h6>{IndicesInfoHead.segue} </h6> <br></br>
+      {/* specifically render core vars table */}
+      <h5>{CoreVarsContents.category}</h5>
+      <Row>
+        <Col>
+          <Table striped size="sm">
+            <thead>
+              <tr>
+                <th>code</th>
+                <th>index</th>
+                <th>unit</th>
+              </tr>
+            </thead>
+            <tbody>
+              {CoreVarsContents["tb1"].map((corevar, i) => (
+                <tr key={i}>
+                  <td>{corevar.value}</td>
+                  <td>{corevar.label}</td>
+                  <td>{corevar.unit}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+
+        <Col>
+          <Table striped size="sm">
+            <thead>
+              <tr>
+                <th>code</th>
+                <th>index</th>
+                <th>unit</th>
+              </tr>
+            </thead>
+            <tbody>
+              {CoreVarsContents["tb2"].map((corevar, i) => (
+                <tr key={i}>
+                  <td>{corevar.value}</td>
+                  <td>{corevar.label}</td>
+                  <td>{corevar.unit}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <span>*mm of liquid water equivalent</span>
+        </Col>
+      </Row>
+      {/* All other indices */}
+      {IndicesInfoContents.map((entry, i) => (
+        <Row className="indices">
+          <div key={i}>
+            <h5>
+              <u>{entry.category}</u>
+            </h5>
+            <p className="foreword">{entry.foreword}</p>
+
+            {entry["options"].map((content, i) => (
+              <p key={i} className="contents">
+                <b>{content.label}</b> ({content.value}):{" "}
+                <Latex displayMode={true}>{content.description}</Latex>{" "}
+                {content.link && <a href={content.link}>See here</a>}
+                <Image
+                  fluid="true"
+                  src={`${process.env.PUBLIC_URL}${content.image}`}
+                  alt=""
+                />
+              </p>
+            ))}
+          </div>
+        </Row>
       ))}
     </Container>
   );
