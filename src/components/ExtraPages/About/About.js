beaa2bfafd2b70ce7031c7ef2367c5f45a54fb3a
acholyn <amandaholyn@gmail.com>
2022-03-14 18:39:44 +0000

    reconfigured about pages
    hardcoded contents; removed contents folder
    removed team page; will link to project landing page from a link

--- src/components/ExtraPages/About/About.js
+++ src/components/ExtraPages/About/About.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import "../ExtraPages.css";
-import { AboutInfo } from "./Contents/AboutInfo";
 
 export default function About() {
   return (
     <Container className="about">
       <div>
-        <h1>{AboutInfo.title}</h1>
+        <h1>About UK CORDEX</h1>
         <p>
-          <a href={AboutInfo.links[1]}>
+          <a href="https://www.metoffice.gov.uk/research/approach/collaboration/ukcp/about">
             The Met Office's UK Climate Projections (UKCP18)
-          </a>
+          </a>{" "}
           provide several different products giving information about the future
           climate of the UK, including projections at global (60km), regional
           (12km) and local (2.2km) scales. Although the regional projections are
@@ -22,13 +21,22 @@ export default function About() {
           with current scientific understanding. A more comprehensive assessment
           of potential climate futures for the UK can be obtained by
           supplementing the UKCP18 regional projections with information from{" "}
-          <a href={AboutInfo.links[2]}>the EuroCORDEX ensemble</a> at the same
-          12km resolution. This combined set will allow a better understanding
-          of structural uncertainty in UK climate projections and substantially
-          enhances the information base available to support future climate
-          change risk assessments.
+          <a href="https://euro-cordex.net/060374/index.php.en">
+            the EuroCORDEX ensemble
+          </a>{" "}
+          at the same 12km resolution. This combined set will allow a better
+          understanding of structural uncertainty in UK climate projections and
+          substantially enhances the information base available to support
+          future climate change risk assessments.
         </p>
-        <p>{AboutInfo.contents[1]}</p>
+        <p>
+          This plot explorer is designed to enable rapid visualisation of the
+          key structures of the model outputs in the UKCP18 projections and
+          EuroCORDEX ensembles, along with their driving models, for a large
+          number of weather variables and derived indices of climate over the
+          UK. This enables users both to evaluate the outputs, and to choose
+          appropriate subsets of model runs for use in their own applications.
+        </p>
       </div>
     </Container>
   );
