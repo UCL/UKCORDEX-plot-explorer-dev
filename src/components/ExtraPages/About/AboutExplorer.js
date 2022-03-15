4af78646627d9df818d5447a3f6ecdea71a960ba
acholyn <amandaholyn@gmail.com>
2022-03-15 10:38:29 +0000

    added skeleton for aboutexplorer page

--- src/components/ExtraPages/About/AboutExplorer.js
+++ src/components/ExtraPages/About/AboutExplorer.js
@@ -1,1 +1,12 @@
-export default function AboutExplorer() {}
+import { Container } from "react-bootstrap";
+
+export default function AboutExplorer() {
+  return (
+    <Container className="about">
+      <div>
+        <h1>About UK CORDEX Plot Explorer</h1>
+        <p></p>
+      </div>
+    </Container>
+  );
+}
