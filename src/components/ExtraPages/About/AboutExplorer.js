cadfc9e5cd498754d7f2741f634e1afd83371e6f
Amanda Ho-Lyn <72266023+acholyn@users.noreply.github.com>
2022-03-17 08:58:16 +0000

    Update src/components/ExtraPages/About/AboutExplorer.js
    will update to the correct link in due course
    
    Co-authored-by: Raquel Alegre <r.alegre@ucl.ac.uk>

--- src/components/ExtraPages/About/AboutExplorer.js
+++ src/components/ExtraPages/About/AboutExplorer.js
@@ -5,7 +5,7 @@ export default function AboutExplorer() {
     <Container className="about">
       <div>
         <h1>About UK CORDEX Plot Explorer</h1>
-        <p></p>
+        <p>This section will have a short summary of what the plot explorer is and a link to https://github-pages.ucl.ac.uk/UKCORDEX-plot-explorer-dev/#/how-to-use for further information on how to use it.</p>
       </div>
     </Container>
   );
