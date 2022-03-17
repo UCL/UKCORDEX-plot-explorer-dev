6fe02ab3f266aa7ec9e6e1daf95c3bf9c8131fef
acholyn <amandaholyn@gmail.com>
2022-03-17 17:10:14 +0000

    updated test to look for correct text

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,13 @@
 import { render, screen } from "@testing-library/react";
-import { HashRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
-
 test("Renders Title", () => {
-  render(<HashRouter>
-           <App />
-         </HashRouter>
+  render(
+    <HashRouter>
+      <App />
+    </HashRouter>
   );
-  const linkElement = screen.getByText("UK CORDEX Plot Explorer");
+  const linkElement = screen.getByText("UKCORDEX Plot Explorer");
   expect(linkElement).toBeInTheDocument();
 });
