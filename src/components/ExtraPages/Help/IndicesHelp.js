e74f7494f5f18fcf78cae6054f6580967ff476cf
clairbarnes <clair.with.no.e@gmail.com>
2022-03-24 13:18:43 +0000

    Import katex css file

--- src/components/ExtraPages/Help/IndicesHelp.js
+++ src/components/ExtraPages/Help/IndicesHelp.js
@@ -3,6 +3,7 @@ import { Container, Table, Row, Col, Image, Nav } from "react-bootstrap";
 import "../ExtraPages.css";
 import "../HelpNav.css";
 import { InlineMath } from "react-katex";
+import 'katex/dist/katex.min.css';
 import { HashLink } from "react-router-hash-link";
 
 function IndicesHelp() {
