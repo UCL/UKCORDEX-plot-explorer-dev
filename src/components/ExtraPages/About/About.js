4cab5471ac6200bf0d9abbbe81ac060478b67822
clairbarnes <clair.with.no.e@gmail.com>
2022-03-29 17:51:13 +0100

    About pages modified to reflect UKCORDEX project page

--- src/components/ExtraPages/About/About.js
+++ src/components/ExtraPages/About/About.js
@@ -8,34 +8,37 @@ export default function About() {
       <div>
         <h1>About the UKCORDEX project</h1>
         <p>
-          The Met Office's{" "}
+          The Met Office's 2018{" "}
           <a href="https://www.metoffice.gov.uk/research/approach/collaboration/ukcp/about">
-            UK Climate Projections (UKCP18)
+            UK Climate Projections
           </a>{" "}
-          provide several different products giving information about the future
+          (UKCP18) provide several different products giving information about the future
           climate of the UK, including projections at global (60km), regional
           (12km) and local (2.2km) scales. Although the regional projections are
-          proving beneficial in understanding future climate risks, these were
+          proving beneficial in understanding future climate risks to the UK, these were
           derived by perturbing the physical parameters of a single global
           climate model (GCM) and a single regional climate model (RCM), and so
-          may not sample the full range of possible futures that is consistent
-          with current scientific understanding. A more comprehensive assessment
-          of potential climate futures for the UK can be obtained by
-          supplementing the UKCP18 regional projections with information from
-          the{" "}
-          <a href="https://euro-cordex.net/060374/index.php.en">EuroCORDEX</a>{" "}
-          ensemble at the same 12km resolution. This combined set will allow a
-          better understanding of structural uncertainty in UK climate
-          projections and substantially enhances the information base available
-          to support future climate change risk assessments.
+          may not sample the full range of uncertainty that is consistent
+          with current scientific understanding.
         </p>
         <p>
-          This plot explorer is designed to enable rapid visualisation of the
-          key structures of the model outputs in the UKCP18 projections and the
-          EuroCORDEX ensembles, along with their driving models, for a large
-          number of weather variables and derived indices of climate over the
-          UK. This enables users both to evaluate the outputs, and to choose
-          appropriate subsets of model runs for use in their own applications.
+          This research project extends the UKCP18 suite of climate projections by augmenting
+          them with information from a broader range of climate simulations at the same 12km resolution,
+          obtained from the <a href="https://euro-cordex.net/060374/index.php.en">EuroCORDEX downscaling experiment</a>.
+          The additional simulations provide a more comprehensive sampling of uncertainty in high-resolution UK climate projections.
+          In turn, this offers the potential to develop better informed strategies for adapting to, and mitigating the effects of, future weather and climate.
+        </p>
+        <p>As well as providing access to additional climate projections for the UK,
+          the project aims to provide insights into the physical plausibility of the various simulations;
+          to assess the value added by considering high-resolution information; to identify
+          the dominant sources of uncertainty in future projections of a variety of weather indices;
+          and to assess the extent to which the existing UKCP ensemble provides a decision-relevant
+          characterisation of this uncertainty. To achieve these challenging objectives,
+          the project takes a multidisciplinary approach combining expertise in climate modelling,
+          modern statistics and uncertainty quantification, and software engineering.
+        </p>
+        <p>
+        The project runs from October 2019 until October 2022, and is funded under the <a href="https://www.ukclimateresilience.org/">UK Climate Resilience Programme</a>.
         </p>
       </div>
     </Container>
--- src/components/ExtraPages/About/AboutExplorer.js
+++ src/components/ExtraPages/About/AboutExplorer.js
@@ -7,9 +7,16 @@ export default function AboutExplorer() {
       <div>
         <h1>About the UKCORDEX Plot Explorer</h1>
         <p>
-          This analysis and visualisation tool is designed to allow rapid exploration of the UKCORDEX projections.
-          Users can select climate indices, seasons and time periods of interest and can compare the various model outputs, 
+          This analysis and visualisation tool is designed to allow rapid exploration of the
+          key structures of the model outputs in the UKCP18 projections and the
+          EuroCORDEX ensembles, along with their driving models, for a large
+          number of weather variables and derived indices of climate over the UK.
+          Users can select climate indices, seasons and time periods of interest and can compare the various model outputs,
           produce maps of projections and historical biases in these indices, and quantify the dominant sources of variation and uncertainty within the projections.
+          This enables users both to evaluate the outputs, and to choose
+          appropriate subsets of model runs for use in their own applications.
+        </p>
+        <p>
           The plots and underlying data are downloadable directly from the plot explorer.
         </p>
       </div>
