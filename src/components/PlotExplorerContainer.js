9249d88a07c377573268ed634346a03ad84ca357
clairbarnes <clair.with.no.e@gmail.com>
2022-03-29 17:04:53 +0100

    ?Fixed links to homepage header

--- src/components/PlotExplorerContainer.js
+++ src/components/PlotExplorerContainer.js
@@ -9,6 +9,7 @@ import "./PlotExplorerContainer.css";
 import DownloadDataButton from "./DownloadDataButton";
 import FileCheck from "./Utils/FileCheck";
 import DisplayWarnings from "./DisplayWarnings";
+import { Link } from "react-router-dom";
 
 function PlotExplorerContainer() {
   /* This is the entry point to the main page. It handles state passing from
@@ -66,7 +67,7 @@ function PlotExplorerContainer() {
           To start, select one of each of the following parameters (each dropdown also allows as-you-type filtering).
           Your plot(s) will be shown when at least one item has been selected in each dropdown.
           To clear all selections, refresh the page. <br></br>
-          The reference guide gives more information on the available <Link to="/indices-help">indices</Link>, <HashLink to="/plot-help">plot types</HashLink>, and <Link to="/time-help">time periods</Link>, as well as details of the <Link to="/data-help">sources of climate data used</Link>.<br></br>
+          The reference guide gives more information on the available <Link to="/indices-help">indices</Link>, <Link to="/plot-help">plot types</Link>, and <Link to="/time-help">time periods</Link>, as well as details of the <Link to="/data-help">sources of climate data used</Link>.<br></br>
           For a suggested order of analyses, see the <Link to="/how-to-use">usage guide</Link>.
         </p>
       </Alert>
