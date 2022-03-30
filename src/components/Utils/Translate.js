3e82bcd30ddf35a57b11d915ab7520685a24daec
clairbarnes <clair.with.no.e@gmail.com>
2022-03-30 20:14:34 +0100

    Revert "Change to title string for plots"
    This reverts commit cd68bc28062b34a40c9b27e3e80a32d6dd83a6ce.

--- src/components/Utils/Translate.js
+++ src/components/Utils/Translate.js
@@ -54,7 +54,7 @@ export function makeTitle(plot) {
   }
 
   // temporarily removing pvarTitle until we can figure out why it doesn't display properly
-  let titleString = `${ptypeTitle} ${ctype} ${seasonTitle} ${dict.pvar} ${periodTitle}`;
+  let titleString = `${regionTitle} ${ptypeTitle} ${ctype} ${dict.pvar} ${pname}`;
   return titleString;
 }
 
