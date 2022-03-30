78aadbaf59992c8727aac7cfabec8c97a55af64d
clairbarnes <clair.with.no.e@gmail.com>
2022-03-30 16:18:41 +0100

    Minor tweaks to download text

--- src/components/ExtraPages/Help/DownloadHelp.js
+++ src/components/ExtraPages/Help/DownloadHelp.js
@@ -13,7 +13,9 @@ export default function DownloadHelp() {
         <h1>Using the downloaded data</h1>
         <p>The plot explorer provides the option to download the data used to produce the plots, as well as the plots themselves.
           <br></br>
-          The data is provided in netcdf4 format, and each file contains only the data used to produce the selected plot.
+          The data is provided in netcdf4 format, and each file contains only the data used to produce the selected plot; however, the files 
+          are designed to in such a way that data from different seasons or time periods can be easily concatenated together for further analysis.
+          <br></br>
           Jupyter notebooks containing the Python code used to produce the plots are currently in development and will be provided via Binder.
         </p>
         {/* Also add notebooks on how to combine files eg. to get relative changes or recreate absolute values of indices? */}
