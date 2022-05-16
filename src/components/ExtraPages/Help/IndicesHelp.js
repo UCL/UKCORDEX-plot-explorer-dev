ef556caf8dda7f4acbe247591c5f58eff15d73d5
clairbarnes <clair.with.no.e@gmail.com>
2022-05-16 10:30:24 +0100

    r95ptot / r99ptot description corrected

--- src/components/ExtraPages/Help/IndicesHelp.js
+++ src/components/ExtraPages/Help/IndicesHelp.js
@@ -635,16 +635,15 @@ function IndicesHelp() {
                 percentile
               </b>
               , <InlineMath math={"\\texttt{r95ptot}"} />: Let{" "}
-              <InlineMath math={"q_{95}(\\texttt{pr}_{rj})"} /> be the 95th
-              percentile of daily wet-day precipitation during all seasons{" "}
-              <InlineMath math={"j"} /> (eg. during all winters) in the
-              reference period <InlineMath math={"r"} /> (1980-2010). The proportion of the total
-              precipitation falling on days exceeding the 95th percentile in season{" "}
+              <InlineMath math={"q_{95}(\\texttt{pr}_{j})"} /> be the 95th
+              percentile of daily wet-day precipitation during all seasons of interest (eg. during all winters)
+              in the period <InlineMath math={"j"} />. The proportion of the total
+              precipitation falling on days exceeding the 95th percentile in the season of interest for period{" "}
               <InlineMath math={"j"} /> is the sum of{" "}
               <InlineMath
-                math={"\\texttt{pr}_{ij} \\geq q_{95}(\\texttt{pr}_{r})"}
+                math={"\\texttt{pr}_{ij} \\geq q_{95}(\\texttt{pr}_{j})"}
               />{" "}
-              divided by <InlineMath math={"\\texttt{prcptot}"} /> for the same period.
+              divided by <InlineMath math={"\\texttt{prcptot}"} /> for the same period and season.
             </p>
             <p className="contents">
               <b>
@@ -653,15 +652,14 @@ function IndicesHelp() {
               </b>
               , <InlineMath math={"\\texttt{r99ptot}"} />: Let{" "}
               <InlineMath math={"q_{99}(\\texttt{pr}_{rj})"} /> be the 99th
-              percentile of daily wet-day precipitation during all seasons{" "}
-              <InlineMath math={"j"} /> (eg. during all winters) in the
-              reference period <InlineMath math={"r"} /> (1980-2010). The proportion of the total
-              precipitation exceeding the 99th percentile in season{" "}
+              percentile of daily wet-day precipitation during all seasons of interest (eg. during all winters)
+              in the period <InlineMath math={"j"} />. The proportion of the total
+              precipitation falling on days exceeding the 99th percentile in season{" "}
               <InlineMath math={"j"} /> is the sum of{" "}
               <InlineMath
-                math={"\\texttt{pr}_{ij} \\geq q_{99}(\\texttt{pr}_{r})"}
+                math={"\\texttt{pr}_{ij} \\geq q_{99}(\\texttt{pr}_{j})"}
               />{" "}
-              divided by <InlineMath math={"\\texttt{prcptot}"} /> for the same period.
+              divided by <InlineMath math={"\\texttt{prcptot}"} /> for the same period and season.
             </p>
             <p className="contents">
               <b>Simple daily precipitation intensity index </b>,{" "}
