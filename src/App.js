98b627258e021b0b03a76c0b9d24624cedb4bbbb
Raquel Alegre <r.alegre@ucl.ac.uk>
2022-05-19 22:38:27 +0100

    Initial attempt at counting visitors.

--- src/App.js
+++ src/App.js
@@ -12,8 +12,13 @@ import Usage from "./components/ExtraPages/Help/Usage";
 import AboutExplorer from "./components/ExtraPages/About/AboutExplorer";
 import IndicesCalculation from "./components/ExtraPages/About/IndicesCalculation";
 import DownloadHelp from "./components/ExtraPages/Help/DownloadHelp";
+import ReactGA from 'react-ga';
 
 function App() {
+
+  ReactGA.initialize('UA-228675619-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     // rendering just the menu header means it'll display on every page, each route is different
     // Everything should be wrapped in a div
