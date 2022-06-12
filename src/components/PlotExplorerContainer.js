73b69e4aa1594288e547f35e4e377b7be21de230
Raquel Alegre <r.alegre@ucl.ac.uk>
2022-06-12 15:38:43 +0100

    Removed some unused debug prints.

--- src/components/PlotExplorerContainer.js
+++ src/components/PlotExplorerContainer.js
@@ -55,7 +55,6 @@ function PlotExplorerContainer() {
         return;
       }
       setImages(images);
-      console.log(images);
       setDataFiles(datafiles);
     }
   }, [plotvars, seasons, periods, plottypes, regions]);
