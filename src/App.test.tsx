ea6070a8603c17950a2fe33319c1a4fae03d4c5e
Amanda Ho-Lyn <amandaholyn@gmail.com>
2022-06-24 10:57:30 +0100

    moved testmode to app.js

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
-import ReactGA from "react-ga";
 
 test("Renders Title", () => {
-  ReactGA.initialize("UA-228675619-1", { testMode: true });
-  ReactGA.pageview(window.location.pathname + window.location.search);
   render(
     <HashRouter>
       <App />
